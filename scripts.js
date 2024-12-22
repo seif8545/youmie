fab05aac42f8262f4f54f67a28ed457d3919e1ec
seif8545 <seifomaraly123@gmail.com>
2024-12-22 14:09:40 +0200

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,7 +1,14 @@
 function toggleSidebar() {
-    document.getElementById('sidebar').classList.toggle('show');
+    const sidebar = document.getElementById('sidebar');
+    const toggleButton = document.querySelector('.toggle-btn');
+    
+    sidebar.classList.toggle('show');
+    if (sidebar.classList.contains('show')) {
+        toggleButton.style.left = '265px'; // Adjust button position when sidebar is shown
+    } else {
+        toggleButton.style.left = '15px'; // Reset button position when sidebar is hidden
+    }
 }
-
 
 function navigateTo(page) {
     window.location.href = page;
